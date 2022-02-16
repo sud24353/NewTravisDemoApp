5840f3335ee95b4b2f229d2f2d75416cd496cbd9
sudipta bhowmik <sbhowmik163@alivecor.com>
2022-02-16 11:13:32 +0530

    index update

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require("express")
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('test')
+    res.send('test..')
 }).listen(process.env.PORT || 5000)
+
+// const http = require('http')
+
+// const proxy = http.createServer('/', (req, res) => {
+//     res.end('test')
+// })
+// proxy.listen(5000, '127.0.0.1' ,() => console.log('server listening to.. '))
